96bc0fcb05869e384c9691d3d8a19afd2d909d61
Meir Shamay <meir.shamay@gmail.com>
2014-07-21 13:32:24 +0300

    package name, core use, config flags, fix error

--- pcap2json.go
+++ pcap2json.go
@@ -1,53 +1,58 @@
 package main
 
 import (
-	"github.com/akrennmair/gopcap"
-	"fmt"
-	"os"
 	"bufio"
 	"bytes"
-	"json"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/akrennmair/gopcap"
+	"net/http"
+	"os"
+	"runtime"
 	"strings"
-	"http"
+	"time"
 )
 
-var out *bufio.Writer
-var errout *bufio.Writer
+var (
+	out    *bufio.Writer
+	errout *bufio.Writer
+	device int
+	filter string
+)
 
-// structure of JSON-serialised packets 
+// structure of JSON-serialised packets
 type RequestPacket struct {
 	SrcIp    string
 	DestIp   string
 	SrcPort  uint16
 	DestPort uint16
-	Time     string
+	Time     time.Time
 	Flags    string
 	// TODO: consider providing alternate for serialisation
-	Request  *http.Request
+	Request *http.Request
 }
 
 // Begin capturing network traffic, returning a handle on the capture which can
 // be used to process captured packets.
 func OpenCaptureOrDie() *pcap.Pcap {
 	devs, err := pcap.Findalldevs()
-	if err != "" {
+	fmt.Println(devs[0])
+	if err != nil {
 		fmt.Fprintf(errout, "pcap2json: couldn't find any devices: %s\n", err)
 	}
 	if 0 == len(devs) {
 		os.Exit(1)
 	}
 
-	// TODO: support passing preferred device through on the cmd-line
-	h, err := pcap.Openlive(devs[0].Name, 65535, true, 0)
+	h, err := pcap.Openlive(devs[device].Name, 65535, true, 0)
 	if h == nil {
 		fmt.Fprintf(errout, "pcap2json: %s\n", err)
 		errout.Flush()
 	}
 
-	// TODO: support passing filter through on the cmd-line
-	expr := "not port 22"
-	ferr := h.Setfilter(expr)
-	if ferr != "" {
+	ferr := h.Setfilter(filter)
+	if ferr != nil {
 		fmt.Fprintf(out, "pcap2json: %s\n", ferr)
 		out.Flush()
 	}
@@ -75,34 +80,44 @@ func SerialisePacket(pkt *pcap.Packet, enc *json.Encoder) {
 	httpMethods := [...]string{"OPTIONS", "GET", "HEAD", "POST", "PUT", "DELETE", "TRACE", "CONNECT"}
 
 	// TODO: IPv6
-	if hdr, ok := pkt.Headers[0].(*pcap.Iphdr); ok {
-		if tcpHdr, ok := pkt.Headers[1].(*pcap.Tcphdr); ok {
-			// TODO: Consider peeking at other ports
-			if tcpHdr.DestPort == 80 {
-				pktString := PacketAsString(pkt)
-				for _, verb := range httpMethods {
-					if strings.Contains(pktString, verb) {
-						unparsedReqs := strings.Split(pktString, verb)
-						for _, unparsed := range unparsedReqs {
-							req, err := http.ReadRequest(bufio.NewReader(strings.NewReader(verb + unparsed)))
-							// TODO: serialise details of packets we fail to parse
-							if err == nil {
-								rp := RequestPacket{hdr.SrcAddr(), hdr.DestAddr(), tcpHdr.SrcPort, tcpHdr.DestPort, pkt.TimeString(), tcpHdr.FlagsString(), req}
-								enc.Encode(rp)
+	if pkt.Headers != nil {
+		if hdr, ok := pkt.Headers[0].(*pcap.Iphdr); ok {
+			if tcpHdr, ok := pkt.Headers[1].(*pcap.Tcphdr); ok {
+				// TODO: Consider peeking at other ports
+				if tcpHdr.DestPort == 80 {
+					pktString := PacketAsString(pkt)
+					for _, verb := range httpMethods {
+						if strings.Contains(pktString, verb) {
+							unparsedReqs := strings.Split(pktString, verb)
+							for _, unparsed := range unparsedReqs {
+								req, err := http.ReadRequest(bufio.NewReader(strings.NewReader(verb + unparsed)))
+								// TODO: serialise details of packets we fail to parse
+								if err == nil {
+									rp := RequestPacket{hdr.SrcAddr(), hdr.DestAddr(), tcpHdr.SrcPort, tcpHdr.DestPort, pkt.Time, tcpHdr.FlagsString(), req}
+									enc.Encode(rp)
+								}
 							}
 						}
 					}
 				}
+				out.Flush()
 			}
-			out.Flush()
 		}
 	}
-
 }
 
-// Open a capture session, attempt to parse any requests to port 80 and 
+// Open a capture session, attempt to parse any requests to port 80 and
 // serialise to stdout as JSON.
 func main() {
+	flag.IntVar(&device, "device", 0, "device/interface to use for sniffing data")
+	flag.StringVar(&filter, "filter", "not port 22", "filter expression for source and destination addresses, and Ethernet-like packet types")
+
+	// For better sniffing performance we can utilize the machine cores
+	goMaxProcs := os.Getenv("GOMAXPROCS")
+	if goMaxProcs == "" {
+		runtime.GOMAXPROCS(runtime.NumCPU())
+	}
+
 	out = bufio.NewWriter(os.Stdout)
 	errout = bufio.NewWriter(os.Stderr)
 
